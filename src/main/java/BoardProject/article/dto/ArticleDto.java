39a0da411581edff195699229b34815d77d9164a
r2alize <redmoon425@naver.com>
2024-10-01 18:11:44 +0900

    feat : ArticleDto ResponseDto 추가

--- src/main/java/BoardProject/article/dto/ArticleDto.java
+++ src/main/java/BoardProject/article/dto/ArticleDto.java
@@ -2,6 +2,8 @@ package BoardProject.article.dto;
 
 import lombok.AllArgsConstructor;
 
+import java.util.Date;
+
 @AllArgsConstructor
 public class ArticleDto {
 
@@ -10,11 +12,29 @@ public class ArticleDto {
     private String body;
 
 
-    public static class Post { // Client에서 Article 생성할 때 입력하는 정보
+    public class Post { // Client에서 Article 생성할 때 입력하는 정보
 
         private String title;
 
         private String body;
+
+
+    }
+
+    public class Response {
+
+
+        private String author;
+
+        private String title;
+
+        private String body;
+
+        // 메타데이터
+        private Date createdAt;
+        private Date modifiedAt;
+
+
 
     }
 
