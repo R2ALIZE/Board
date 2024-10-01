17280375c5db53731e352c64184050e7e198fe42
r2alize <redmoon425@naver.com>
2024-10-01 18:11:19 +0900

    feat : ArticleMapper Article -> ArticleDto.Response 추가

--- src/main/java/BoardProject/article/mapper/ArticleMapper.java
+++ src/main/java/BoardProject/article/mapper/ArticleMapper.java
@@ -13,4 +13,6 @@ public interface ArticleMapper {
     // DTO - ENTITY 간의 변환 책임을 맡은 Mapper 클래스
 
     Article convertArticlePostDtoToArticle (ArticleDto.Post articlePostDto);
+
+    ArticleDto.Response convertArticleToArticleResponseDto (Article article);
 }
