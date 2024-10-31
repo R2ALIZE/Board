ec10fd8eceb386cbf70ba95830df44f3be82e708
r2alize <redmoon425@naver.com>
2024-10-31 18:45:12 +0900

    :adhesive_bandage: Fix : 암호화 전략 패턴 코드 일부 수정

--- src/main/java/boardProject/global/auth/encryption/context/EncryptionContext.java
+++ src/main/java/boardProject/global/auth/encryption/context/EncryptionContext.java
@@ -1,22 +1,39 @@
 package boardProject.global.auth.encryption.context;
 
 import boardProject.global.auth.encryption.strategy.EncryptionStrategy;
+import boardProject.global.exception.BusinessLogicException;
+import boardProject.global.exception.StatusCode;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+
 @Setter
 public class EncryptionContext {
 
     private EncryptionStrategy encryptionStrategy;
 
+    public EncryptionContext(EncryptionStrategy encryptionStrategy) {
+        this.encryptionStrategy = encryptionStrategy;
+    }
 
-    public String encrypt(String plainText) {
+    public String encrypt(String plainText)
+            throws GeneralSecurityException, UnsupportedEncodingException {
+
+        if (encryptionStrategy == null) {
+            throw new BusinessLogicException(StatusCode.ENCRYPTION_STRATEGY_NOT_SET);
+        }
+
         return encryptionStrategy.encrypt(plainText);
     }
 
-    public String decrypt(String encryptedText) {
+    public String decrypt(String encryptedText)
+            throws GeneralSecurityException, UnsupportedEncodingException {
+
+        if (encryptionStrategy == null) {
+            throw new BusinessLogicException(StatusCode.ENCRYPTION_STRATEGY_NOT_SET);
+        }
+
         return encryptionStrategy.decrypt(encryptedText);
     }
 
