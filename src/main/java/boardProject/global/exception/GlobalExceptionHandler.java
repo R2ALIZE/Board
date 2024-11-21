3e41b41971e07d45ed6a4827f62342015476e678
r2alize <redmoon425@naver.com>
2024-11-21 18:22:19 +0900

    :sparkles: Feat: Controller에서 exception 발생 시에 응답 헤더에 mvc exception 항목 추가

--- src/main/java/boardProject/global/exception/GlobalExceptionHandler.java
+++ src/main/java/boardProject/global/exception/GlobalExceptionHandler.java
@@ -1,11 +1,12 @@
 package boardProject.global.exception;
 
+import boardProject.global.response.ErrorResponse;
 import boardProject.global.response.Response;
 import boardProject.global.util.slack.SlackUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,16 +19,23 @@ import java.io.IOException;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
-    @Autowired
-    private SlackUtil slackUtil;
+    private final SlackUtil slackUtil;
+
+    private final HttpServletRequest request;
+
+    private final HttpServletResponse response;
 
     @ExceptionHandler(BusinessLogicException.class)
     public Response<ErrorResponse> handleBusinessLogicException
-            (BusinessLogicException be, HttpServletRequest req) throws IOException {
+            (BusinessLogicException be) throws IOException {
 
-        log.warn("handleBusinessLogicException : {}",be.getMessage());
+        log.error("handleBusinessLogicException : {}",be.getMessage());
+
+        slackUtil.sendAlert(be,request);
+        response.setHeader("Mvc Exception","true");
 
         return new Response<>(ErrorResponse.of(be.getStatusCode()));
     }
@@ -35,11 +43,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Response<ErrorResponse> handleMethodArgumentNotValidException
-            (MethodArgumentNotValidException me, HttpServletRequest req) throws IOException {
+            (MethodArgumentNotValidException me) {
 
         log.warn("handleMethodArgumentNotValidException : {}", me.getMessage());
-
-        slackUtil.sendAlert(me,req);
+        response.setHeader("Mvc Exception","true");
 
         return new Response<>(ErrorResponse.of(me.getBindingResult()));
 
@@ -50,27 +57,25 @@ public class GlobalExceptionHandler {
     public Response<ErrorResponse> handleMethodValidationException (HandlerMethodValidationException he) {
 
         log.warn("handleMethodValidationException : {}", he.getMessage());
+        response.setHeader("Mvc Exception","true");
 
         return new Response<>(ErrorResponse.of(he));
     }
 
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Response<ErrorResponse> handleAllException
-            (Exception e, HttpServletRequest req) throws IOException {
+            (Exception e) throws IOException {
 
-        log.warn("handleException : {}, cause : {}", e.getMessage(), e.getCause());
+        log.error("handleException : {}, cause : {}", e.getMessage(), e.getCause());
 
-        slackUtil.sendAlert(e,req);
+        slackUtil.sendAlert(e,request);
+        response.setHeader("Mvc Exception","true");
 
         return new Response<>(ErrorResponse.of(e));
 
     }
-
-
-
-
 
 
 
