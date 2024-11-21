8a381c0a9c49700b6a0cc8a9ef38ac0ebcc5a07b
r2alize <redmoon425@naver.com>
2024-11-21 18:08:31 +0900

    :art: Style : ResidentNumFormatCheck 코드 구조 개선

--- src/main/java/boardProject/global/validation/ResidentNumFormatCheck.java
+++ src/main/java/boardProject/global/validation/ResidentNumFormatCheck.java
@@ -1,22 +1,16 @@
 package boardProject.global.validation;
 
-import boardProject.global.aop.ResidentNumFormat;
+import boardProject.global.aop.format.ResidentNumFormat;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Aspect
 @Component
 public class ResidentNumFormatCheck implements ConstraintValidator<ResidentNumFormat,String> {
-
-
-    private final int TOTAL_LENGTH = 13;
-    private final int BIRTH_PART_LENGTH = 6;
-    private final int GENDER_PART_LENGTH = 7;
-    private final int END_OF_MONTH = 12;
-    private final int END_OF_DAY = 31;
-
 
 
     /** dto에서는 주민번호는 - 제외 13자리로 받음 **/
@@ -24,12 +18,14 @@ public class ResidentNumFormatCheck implements ConstraintValidator<ResidentNumFo
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+
         return checkLength(value) && checkBirthPart(value) && checkGenderPart(value);
     }
 
 
     public boolean checkLength (String value) {
-        return value.length() == TOTAL_LENGTH;
+        int totalLength = 13;
+        return value.length() == totalLength;
     }
 
 
@@ -40,18 +36,15 @@ public class ResidentNumFormatCheck implements ConstraintValidator<ResidentNumFo
 
         boolean isValid = true;
 
-
-        if (birthPart.length() != BIRTH_PART_LENGTH) {
-            return false;
-        }
-
         // 주민등록번호 상 월에 해당되는 2자리가 12보다 클 때
-        if (Integer.valueOf(birthPart.substring(2,4)) > END_OF_MONTH) {
+        int END_OF_MONTH = 12;
+        if (Integer.parseInt(birthPart.substring(2,4)) > END_OF_MONTH) {
             return false;
         }
 
         // 주민등록번호 상 일에 해당되는 2자리가 31보다 클 때
-        if (Integer.valueOf(birthPart.substring(4,6)) > END_OF_DAY) {
+        int END_OF_DAY = 31;
+        if (Integer.parseInt(birthPart.substring(4,6)) > END_OF_DAY) {
             return false;
         }
 
@@ -62,18 +55,19 @@ public class ResidentNumFormatCheck implements ConstraintValidator<ResidentNumFo
     public boolean checkGenderPart (String value) {
 
 
-        String genderPart = value.substring(6,value.length());
+        String genderPart = value.substring(6);
 
 
         boolean isValid = true;
 
 
+        int GENDER_PART_LENGTH = 7;
         if (genderPart.length() != GENDER_PART_LENGTH) {
             return false;
         }
 
         // 뒷자리 1번째 번호가 4보다 클 때 (현재 4까지만 사용되고 있음)
-        if (Integer.valueOf(genderPart.substring(0,1)) > 4) {
+        if (Integer.parseInt(genderPart.substring(0,1)) > 4) {
             return false;
         }
 
