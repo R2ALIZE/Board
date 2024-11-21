8a69bd95b41cc00c4d144e16c26a978f674a1361
r2alize <redmoon425@naver.com>
2024-11-21 17:53:31 +0900

    :art: Style: record 클래스로 전환

--- src/main/java/boardProject/global/exception/RequestFieldError.java
+++ src/main/java/boardProject/global/exception/RequestFieldError.java
@@ -1,42 +1,22 @@
 package boardProject.global.exception;
 
-import lombok.Getter;
-import org.springframework.context.MessageSourceResolvable;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.method.ParameterValidationResult;
-import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 
-@Getter
-public class RequestFieldError {
+public record RequestFieldError(String field, String rejectedValue, String reason) {
 
-    private String field;
+    public static List<RequestFieldError> of(BindingResult bindingResult) {
 
-    private String rejectedValue;
-
-    private String reason;
-
-
-    public RequestFieldError(String field, String rejectedValue, String reason) {
-        this.field = field;
-        this.rejectedValue = rejectedValue;
-        this.reason = reason;
-    }
-
-    public static List<RequestFieldError> of (BindingResult bindingResult) {
-
-      return  bindingResult.getFieldErrors().stream()
-                                            .map(error -> new RequestFieldError(
-                                                error.getField(),
-                                                error.getRejectedValue() == null ?
-                                                        "" : error.getRejectedValue().toString(),
-                                                error.getDefaultMessage())
-                                            )
-                                            .collect(Collectors.toList());
+        return bindingResult.getFieldErrors().stream()
+                .map(error -> new RequestFieldError(
+                        error.getField(),
+                        error.getRejectedValue() == null ?
+                                "" : error.getRejectedValue().toString(),
+                        error.getDefaultMessage())
+                )
+                .collect(Collectors.toList());
     }
 }
